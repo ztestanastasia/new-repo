ca1fe79f36b3ce91fbbbaf947eae12b8ca21ed96
ztestanastasia <99515153+ztestanastasia@users.noreply.github.com>
2022-03-24 21:32:15 -0700

    t

---
+++ s3_staticwebsite.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "static" {
+  bucket = "examplebuckettftest"
+  acl    = "private"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
